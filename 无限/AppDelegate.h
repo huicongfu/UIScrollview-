494fff755d50a87eb39ecc033d6f0eb49fc0ff44
huicongfu <347795319@qq.com>
2016-03-15 09:35:53 +0800

    Initial Commit

---
+++ 无限/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  无限
+//
+//  Created by hunuo on 16/3/15.
+//  Copyright © 2016年 hunuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
